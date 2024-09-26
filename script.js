7717b82abb6bcbcfea5be0eb8e6074b3e2b553b2
amrelqalamy <amrelqalamy@gmail.com>
2024-09-26 15:47:09 +0300

    Easy Notes Problem

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const noteInput = document.getElementById("noteInput");
+const notesDiv = document.getElementById("notesDiv");
+const zeroNotes = document.getElementById("zeroNotes");
 
 var notesArrey = [];
 
@@ -9,6 +11,8 @@ function addNote() {
     }else {
         notesArrey.push(newNote);
         noteInput.value = "";
+        saveNotes();
+        showNotes();
         console.log(notesArrey);
     }
 }
@@ -27,3 +31,44 @@ function getNotes() {
         notesArrey = JSON.parse(notesString);
     }
 }
+
+function showNotes() {
+    
+    getNotes();
+    
+    if (notesArrey.length > 0) {
+        zeroNotes.style.display = "none";
+    } else {
+        zeroNotes.style.display = "block";
+    }
+    
+    notesDiv.innerHTML = "";
+
+for (let index = 0; index < notesArrey.length; index++) {
+    const element = notesArrey[index];
+
+    const newDiv = document.createElement("div");
+
+    const newParagraph = document.createElement("p");
+    newParagraph.innerText = element;
+    newDiv.appendChild(newParagraph);
+    
+    const deletIcon = document.getElementById("i");
+    deletIcon.className = "bi bi-trash3";
+    deletIcon.onclick = function () {
+        
+        if (confirm("Are you sure to delete note?") == true) {
+            notesArrey.splice(index, 1);
+            saveNotes();
+            showNotes();
+        }
+    }
+    newDiv.appendChild(deletIcon);
+
+    notesDiv.appendChild(newDiv);
+
+}
+
+}
+
+showNotes()
